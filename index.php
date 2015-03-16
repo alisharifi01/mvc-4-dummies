6a50d3d483bb6fd250e8aeb74db5f03e11537f63
Ali Sharifi <alisharifi01@gmail.com>
2015-03-16 14:40:17 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-
-
-
 include 'conf/app.php';
-//include SMARTY_DIR . 'Smarty.class.php';
 include 'core/autoload.php';
 ErrorHandler::SetHandler(ERROR_TYPES);
 include 'routes.php';
-//
-//$app = new Application();
-//$app->run();
 ?>
